471d61d966268ba8680fbffce986f690dbbb6e29
jackeywells11 <jackeywells11@outlook.com>
2025-05-14 14:26:13 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+            color: 'inherit',
+            a: {
+              color: 'inherit',
+              textDecoration: 'none',
+              fontWeight: '500',
+            },
+            strong: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            code: {
+              color: 'inherit',
+              fontWeight: '400',
+            },
+            h1: {
+              color: 'inherit',
+              fontWeight: '700',
+            },
+            h2: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            h3: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            h4: {
+              color: 'inherit',
+              fontWeight: '600',
+            },
+            blockquote: {
+              color: 'inherit',
+              borderLeftColor: 'inherit',
+            },
+            hr: {
+              borderColor: 'inherit',
+            },
+            ol: {
+              color: 'inherit',
+            },
+            ul: {
+              color: 'inherit',
+            },
+            li: {
+              color: 'inherit',
+            },
+            thead: {
+              color: 'inherit',
+            },
+            tbody: {
+              color: 'inherit',
+            },
+            tr: {
+              borderBottomColor: 'inherit',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+} 
